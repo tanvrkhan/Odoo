520d1aa0f974b6492073d2b7a80540964b2fe778
Ashif Pk <ashifpk1@gmail.com>
2022-12-20 16:30:26 +0530

    [ADD] module added

---
+++ oe_kemoxon_delivery_custom/model/stock_picking.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    bill_date = fields.Date("B/L Date")
+    vessel_name = fields.Char("Vessel Name")
+    delivery_location = fields.Many2one('delivery.location', "Delivery Location")
+    imo_number = fields.Char("IMO Number")
+    delivery_from = fields.Date("Delivery From")
+    delivery_to = fields.Date('Delivry To')
+    truck_transport_details_ids = fields.One2many('truck.transport.details', 'stock_pick_ids', "Truck Details")
+    transporter = fields.Many2one('res.partner', 'Transporter')
+    consignee = fields.Many2one('res.partner', 'Consignee')
+    transporter_payment_terms = fields.Many2one('account.payment.term', 'Transporter Payment Terms')
+    rate = fields.Float('Rate')
+    transport_tolerance = fields.Float('Transport Tolerance')
+
+    @api.onchange('consignee')
+    def _domain_change(self):
+        print('_domain_change_domain_change')
+        domain = []
+        if self.partner_id:
+            if self.partner_id.child_ids:
+                domain = self.partner_id.child_ids.ids
+                domain.append(self.partner_id.id)
+            else:
+                domain.append(self.partner_id.id)
+        print(domain,'domaindomain')
+        return {
+            'domain': {
+                'consignee': [('id', 'in', domain)]}
+        }
+
+
+class DeliveryLocation(models.Model):
+    _name = "delivery.location"
+
+    name = fields.Char("Name")
