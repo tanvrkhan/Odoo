de0ca5432c1f34bd1072ec003abee780197f3364
sadnan-khan-wazir <sadnanhussain2@gmail.com>
2023-01-09 12:48:44 +0500

    [UPL truck_seq]

---
+++ truck_seq/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Truck Sequence",
+    'summary': """Truck Sequence""",
+    'description': """
+        Truck Sequence
+    """,
+    'author': "OdooErp.ae",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'oe_kemoxon_delivery_custom'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ]
+}
