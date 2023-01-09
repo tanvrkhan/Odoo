d061bffa77a64621a07ab5e7e8b86bee08cccab2
sadnan-khan-wazir <sadnanhussain2@gmail.com>
2023-01-09 15:55:15 +0500

    UPL Company Bank Acc

---
+++ contact_bank_acc/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Company Bank Account",
+    'summary': """
+        Company Bank Account on Contact Form
+    """,
+    'description': """
+        Copmany Bank Account On Contact Form
+    """,
+    'author': "OdooErp.ae",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'account', 'contacts', 'sequence_modification'],
+    'data': [
+        'views/views.xml',
+    ],
+}
