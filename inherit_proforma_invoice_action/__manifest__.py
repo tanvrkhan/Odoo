91e2712030927d676035d8f0143282864a169c14
sadnan-khan-wazir <sadnanhussain2@gmail.com>
2023-01-10 20:10:32 +0500

    [UPL] updateProforma invoice action

---
+++ inherit_proforma_invoice_action/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inherit Pro-forma Invoice",
+    'summary': """
+        Inherit Pro-Forma Invoice To change the download name of the report
+    """,
+    'description': """
+        Inherit Pro-Forma Invoice To change the download name of the report
+    """,
+    'author': "odooERP.ae",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'sale'],
+    'data': [
+        'views/views.xml',
+    ]
+}
