f72f88c71020bf2afab7d73b1db6c1facc023ac4
sadnan-khan-wazir <sadnanhussain2@gmail.com>
2023-01-11 21:07:51 +0500

    [UPL] new changes in report

---
+++ oe_kemoxon_delivery_custom/models/payment_term_text.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models
+from bs4 import BeautifulSoup
+
+
+class InheritAccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @staticmethod
+    def get_text_from_html_field(doc):
+        text = ' '.join(BeautifulSoup(doc.invoice_payment_term_id.note, "html.parser").findAll(text=True))
+        return text
+
+
+class InheritSaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @staticmethod
+    def get_text_from_html_field(doc):
+        text = ' '.join(BeautifulSoup(doc.payment_term_id.note, "html.parser").findAll(text=True))
+        return text
