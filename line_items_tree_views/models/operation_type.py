7cfaf17feb2f7e598ace59a6fe2b5a05d082b7c2
=Tanveer Khan <=tk@kemexon.com>
2023-05-25 14:46:30 +0400

    Upgrades 25-05-2023 on stock moves

--- line_items_tree_views/models/operation_type.py
+++ line_items_tree_views/models/operation_type.py
@@ -4,25 +4,44 @@ from odoo import fields, models, api
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    qty_done_incoming = fields.Float(string='Incoming', compute='_compute_qty_done_incoming', digits=(6, 3))
-    qty_done_outgoing = fields.Float(string='Outgoing', compute='_compute_qty_done_outgoing', digits=(6, 3))
-    qty_done_internal_transfer = fields.Float(string='Internal Transfer', compute='_compute_qty_done_internal_transfer', digits=(6, 3))
+    qty_done_internal_transfer = fields.Float(string='Internal Transfer', compute='_compute_qty_done_internal_transfer',
+                                              digits=(6, 3))
+    qty_done_success = fields.Float(
+        string='Incoming',
+        related='qty_done',
+        store=True,
+        help='Field displaying the quantity for the Success condition.'
+    )
+    qty_done_danger = fields.Float(
+        string='Outgoing',
+        store=True,
+        related='qty_done',
+        help='Field displaying the quantity for the Danger condition.'
+    )
+    qty_done_difference = fields.Float(
+        string='Difference',
+        compute='_compute_qty_difference',
+        group_operator='sum',
+        digits=(6, 3)
+    )
 
-    @api.depends('qty_done', 'picking_type_id.code')
-    def _compute_qty_done_incoming(self):
+    @api.depends('qty_done_success', 'qty_done_danger')
+    def _compute_qty_difference(self):
         for record in self:
-            if record.picking_type_id.code == 'incoming':
-                record.qty_done_incoming = record.qty_done
-            else:
-                record.qty_done_incoming = 0.0
+            record.qty_done_difference = record.qty_done_success - record.qty_done_danger
 
-    @api.depends('qty_done', 'picking_type_id.code')
-    def _compute_qty_done_outgoing(self):
-        for record in self:
-            if record.picking_type_id.code == 'outgoing':
-                record.qty_done_outgoing = record.qty_done
-            else:
-                record.qty_done_outgoing = 0.0
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        result = super(StockMoveLine, self).read_group(
+            domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
+
+        for res in result:
+            if '__domain' in res:
+                lines = self.search(res['__domain'])
+                total_qty_done_difference = sum(lines.mapped('qty_done_difference'))
+                res['qty_done_difference'] = total_qty_done_difference
+
+        return result
 
     @api.depends('qty_done', 'picking_type_id.code')
     def _compute_qty_done_internal_transfer(self):
@@ -31,4 +50,3 @@ class StockMoveLine(models.Model):
                 record.qty_done_internal_transfer = record.qty_done
             else:
                 record.qty_done_internal_transfer = 0.0
-
