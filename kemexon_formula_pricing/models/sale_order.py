1a53b811e39a2e1840bea776c6394d7a98a61f38
Tanveer <tanvrkhan@gmail.com>
2023-07-25 10:36:05 +0400

    Added validity and show validity to PFI

--- kemexon_formula_pricing/models/sale_order.py
+++ kemexon_formula_pricing/models/sale_order.py
@@ -5,4 +5,6 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     show_formula_pricing = fields.Boolean('Show Formula Pricing')
+    valid_until = fields.Date('Valid until')
+    show_validity = fields.Boolean('Show validity')
 
