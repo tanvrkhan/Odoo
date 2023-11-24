002a388d874915eefdb49185748d4af688fc13e2
tk-mac <tan.khan@outlook.com>
2023-11-24 13:46:10 +0400

    Error fix partial reconcile errors

--- fendahl_interface/models/models.py
+++ fendahl_interface/models/models.py
@@ -54,6 +54,25 @@ class PostContactsModel(models.Model):
 class PostReconciledPayments(models.Model):
     _inherit = 'account.partial.reconcile'
     
+    sent_to_fendahl= fields.Boolean("Sent to Fendahl")
+    debit_move_type = fields.Selection(selection=[
+            ('entry', 'Journal Entry'),
+            ('out_invoice', 'Customer Invoice'),
+            ('out_refund', 'Customer Credit Note'),
+            ('in_invoice', 'Vendor Bill'),
+            ('in_refund', 'Vendor Credit Note'),
+            ('out_receipt', 'Sales Receipt'),
+            ('in_receipt', 'Purchase Receipt'),
+        ], readonly=True,related='debit_move_id.move_type')
+    credit_move_type= fields.Selection(selection=[
+            ('entry', 'Journal Entry'),
+            ('out_invoice', 'Customer Invoice'),
+            ('out_refund', 'Customer Credit Note'),
+            ('in_invoice', 'Vendor Bill'),
+            ('in_refund', 'Vendor Credit Note'),
+            ('out_receipt', 'Sales Receipt'),
+            ('in_receipt', 'Purchase Receipt'),
+        ], readonly=True,related='debit_move_id.move_type')
     def action_post_payments(self):
         headers = {"Content-Type": "application/json", "Accept": "application/json", "Catch-Control": "no-cache",
                    "Apikey": "268d72e6-5013-4687-8cfa-66d2b633b115"}
@@ -63,7 +82,7 @@ class PostReconciledPayments(models.Model):
         #     # Add other parameters if required by the API
         # }
         company=""
-        match self.debit_move_id.company_id:
+        match self.debit_move_id.company_id.id:
             case 1:
                 company="KEMEXON LTD"
             case 2:
@@ -77,28 +96,44 @@ class PostReconciledPayments(models.Model):
             json_data = {
                 "Accounting_System_Payment_ID": self.credit_move_id.id,
                 "Internal_Company_Code": company,
-                "CounterParty_Code": self.debit_move_id.partner_id.short_name,
-                "Payment_Made_Date": self.credit_move_id.date.isoformat(),
-                "Payment_Due_Date": self.credit_move_id.date.isoformat(),
-                "Payment_Amount": self.credit_move_id.balance,
+                "CounterParty_Code": self.debit_move_id.partner_id.name,
+                "Payment_Made_Date": self.credit_move_id.date.strftime("%d-%m-%Y"),
+                "Payment_Due_Date": self.credit_move_id.date.strftime("%d-%m-%Y"),
+                "Payment_Amount": self.credit_move_id.balance*-1,
                 "Payment_Currency": self.credit_move_id.currency_id.name,
-                "Payment_Allocations": self.debit_move_id.move_id.fusion_reference,
-                "Allocated_Amount": self.amount,
+                "Payment_Allocations": [
+                    {
+                        "Invoice_Master_ID": self.debit_move_id.move_id.fusion_reference,
+                        "Allocated_Amount": self.amount,
+                    }
+                ],
+                
             }
             response = requests.post(url, data=json.dumps(json_data), headers=headers)
+            if(response.status_code==200):
+                self.sent_to_fendahl=True
+            a=1
         elif (self.credit_move_id.move_type == "in_invoice"):
             json_data = {
                 "Accounting_System_Payment_ID": self.debit_move_id.id,
                 "Internal_Company_Code": company,
-                "CounterParty_Code": self.credit_move_id.partner_id.short_name,
+                "CounterParty_Code": self.credit_move_id.partner_id.name,
                 "Payment_Made_Date": self.debit_move_id.move_id.date.isoformat(),
                 "Payment_Due_Date": self.debit_move_id.move_id.date.isoformat(),
                 "Payment_Amount": self.debit_move_id.balance,
                 "Payment_Currency": self.debit_move_id.currency_id.name,
-                "Payment_Allocations": self.credit_move_id.move_id.fusion_reference,
-                "Allocated_Amount": self.amount,
+                "Payment_Allocations": [
+                    {
+                        "Invoice_Master_ID": self.credit_move_id.move_id.fusion_reference,
+                        "Allocated_Amount": self.amount,
+                    }
+                ],
+                
             }
             response = requests.post(url, data=json.dumps(json_data), headers=headers)
+            if (response.status_code == 200):
+                self.sent_to_fendahl = True
+            b=1
     
     def json_serial(obj):
         """JSON serializer for objects not serializable by default json code"""
