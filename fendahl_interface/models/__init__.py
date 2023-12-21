7b791682d3c04c1e1a2174c7c73c3d385bc7c42c
mfaizanyaseen <fy@kemexon.com>
2023-12-21 13:59:52 +0400

    updated endpoints for interface

--- fendahl_interface/models/__init__.py
+++ fendahl_interface/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import models
+from . import payment
+from . import contact
--- oe_kemoxon_delivery_custom/models/account_move.py
+++ oe_kemoxon_delivery_custom/models/account_move.py
@@ -26,6 +26,7 @@ class AccountMove(models.Model):
     fusion_imo = fields.Char('Fusion IMO')
     fusion_vessel_name = fields.Char('Fusion Vessel Name')
     fusion_deal_ref = fields.Char('Fusion Deal Ref')
+    contract_ref = fields.Char('Contract Reference')
     deal_ref = fields.Char("Deal Ref")
     bill_date = fields.Date("B/L Date", related='picking_id.bill_date')
     tt_date = fields.Date("TT Date", related='picking_id.tt_date')
