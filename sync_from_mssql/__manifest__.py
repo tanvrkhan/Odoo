545bfc372a1b5ea894520c68b23aaaaa38fa57f8
tk-mac <tan.khan@outlook.com>
2024-04-19 13:18:12 +0400

    sync from sql added to be tested

---
+++ sync_from_mssql/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Sync From MSSQL/MS SQL SERVER",
+    'version': '16.0.1.0.0',
+    'application': True,
+    'license': 'OPL-1',
+    'installable': True,
+    'category': 'Administration',
+    'author':'dioe',
+    'images': ['static/description/banner.png'],
+    'price':29.99,
+    'currency':'USD',
+    'summary': "Sync From MSSQL/SQL SERVER",    
+    'data': [
+        'security/ir.model.access.csv',
+        'views/message_wizard.xml',
+        'views/dbsync.xml'
+
+
+    ],
+    'external_dependencies': {'python' : ['unixodbc','unixodbc-dev','pyodbc']}
+}
