602a426f52545be00342373712582eceb40ff8cf
tk-mac <tan.khan@outlook.com>
2024-05-16 16:53:49 +0400

    interface working in progress

--- fendahl_interface/models/fusion_sync_history.py
+++ fendahl_interface/models/fusion_sync_history.py
@@ -342,13 +342,13 @@ class FusionSyncHistory(models.Model):
         # Use random.choices to select `length` characters
         random_string = ''.join(random.choices(characters, k=5))
         return random_string
-    def validate_warehouse(self,warehouse,company,nomkey):
-        result = self.env['stock.warehouse'].search([('name', '=', nomkey),('company_id', '=', company)], limit=1)
+    def validate_warehouse(self,warehouse,company):
+        result = self.env['stock.warehouse'].search([('name', '=', warehouse),('company_id', '=', company)], limit=1)
         
         if not result:
             code = self.get_wh_code(warehouse, company)
             result = self.env['stock.warehouse'].create({
-                'name': nomkey,
+                'name': warehouse,
                 'code':code,
                 'company_id': company,
                 'partner_id':company
